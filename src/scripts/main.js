5838a41c4f219179a3503cb4b7128dacea2bddd9
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-03-18 21:06:20 +0100

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (e.target === wall) {
+    const spider = document.querySelector('.spider');
+    const startWidth =
+      (window.innerWidth - wall.clientWidth + spider.clientWidth) / 2;
+    const startHeight =
+      (window.innerHeight - wall.clientHeight + spider.clientHeight) / 2;
+
+    spider.style.left = e.x - startWidth + 'px';
+    spider.style.top = e.y - startHeight + 'px';
+  }
 });
